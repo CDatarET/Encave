126babf69bc55b78e3efcb3ef5abd668c7be5bfd
CdatarET <datar.chaitanya@gmail.com>
2026-03-11 22:19:04 +0000

    .

---
+++ Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        JFrame frame = new JFrame("Encave System");
+        frame.setSize(800,600);
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+    }
+}
