7a555ed8a64d47331512c7777f7fcaadb4f16e0f
CdatarET <datar.chaitanya@gmail.com>
2026-04-02 22:05:33 +0000

    .

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 import javax.swing.*;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello World");
-
         JFrame frame = new JFrame("Encave System");
         frame.setSize(800,600);
         frame.setLayout(null);
@@ -11,5 +12,26 @@ public class Main{
         frame.setLocationRelativeTo(null);
 
         frame.setVisible(true);
+
+        Scanner scan = new Scanner(System.in);
+        HashMap<String, String> map = new HashMap<String, String>();
+        while(true){
+            System.out.print("Enter username: ");
+            String username = scan.nextLine();
+            System.out.print("Enter password: ");
+            String password = scan.nextLine();
+            if(map.get(username) == null){
+                map.put(username, password);
+                System.out.println("Successfully added new user!");
+            }
+            else{
+                if(password != map.get(username)){
+                    System.out.println("Wrong Password");
+                }
+                else{
+                    System.out.println("Welcome back " + username + "!");
+                }
+            }
+        }
     }
 }
